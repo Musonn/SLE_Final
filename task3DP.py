033d8d2b7587f75d9000acff343d0d2719e10685
Musonn <43937020+Musonn@users.noreply.github.com>
2021-12-14 23:20:16 -0500

    Update task3DP.py
    Just need a probability table

--- task3DP.py
+++ task3DP.py
@@ -55,7 +55,7 @@ def get_state(observation):
     return tuple([a-1, b-1, c-1, d-1])
 
 # physical model for DP task
-class DPModel():
+class DPEnv():
     def __init__(self):
         self.gravity = 9.8
         self.masscart = 1.0
@@ -69,7 +69,7 @@ class DPModel():
         self.tau = 0.02  # seconds between state updates
 
         # Angle at which to fail the episode
-        self.theta_threshold_radians = 12 * math.pi / 180
+        self.theta_threshold_radians = 12 * np.pi / 180
         self.x_threshold = 2.4
 
     def step(self, action):
@@ -130,66 +130,75 @@ class DPModel():
 
 # hyper-parameters
 threshold = 0.05 # indecisive
+shape = (3,3,6,6)
+all_actions = 2
+discount = 0.99
 
 # init
-V_value_table = np.zeros((3,3,6,6))
-pi_right = np.full((3,3,6,6), 0.5)
+V_value_table = np.zeros(shape)
+p = np.full(shape+(all_actions,), 0.5)
+#p = np.random.normal(0, 1, shape+(all_actions,))
+policy = np.full(shape, 1)  # a table full of 0 or 1
+pi_right = np.zeros(shape)
 pi_left = pi_right.copy()
-all_actions = 2
-delta = 0
 
-model = DPModel()
+
+
+model = DPEnv()
+
 # policy iteration
-while delta < threshold:
+global Evaluation
+def Evaluation():
     delta = 0
-    obs = env.reset()
+    while delta < threshold:
+        delta = 0
+        obs = env.reset()
+        for a in range(3):
+            for b in range(3):
+                for c in range(6):
+                    for d in range(6):
+                        v = V_value_table[a,b,c,d]
+                        
+                        summ = 0
+                        for sa in range(3):
+                            for sb in range(3):
+                                for sc in range(6):
+                                    for sd in range(6):
+                                        best_move = policy[a,b,c,d]
+                                        summ += p[sa,sb,sc,sd, best_move]*(1 + discount*V_value_table[sa,sb,sc,sd])
+                        V_value_table[a,b,c,d] = summ
+                        
+                        delta = max(delta, abs(v - V_value_table[a,b,c,d]))
+
+# Policy Improvement
+policy_stable = False
+eposoid = 0
+
+while policy_stable is False:
+    difference_count = 0
+    eposoid+=1
+    print(eposoid)
+    policy_stable = True
     for a in range(3):
         for b in range(3):
             for c in range(6):
                 for d in range(6):
-                    v = V_value_table[a,b,c,d]
-                    
-                    p = np.zeros((3,3,6,6))
-                    
-                    best_move = max(pi_right[a,b,c,d], pi_left[a,b,c,d])
-                    count = 0
-                    
-                    #Right
-                    if pi_right[a,b,c,d] == best_move:
-                        if j == 3:
-                            p[a,b,c,d] += 1
-                        else:
-                            p[a,b,c,d+1] += 1
-                        count += 1
-                    #Left
-                    if pi_left[a,b,c,d] == best_move:
-                        if j == 0:
-                            p[a,b,c,d] += 1
-                        else:
-                            p[a,b,c,d-1] += 1
-                        count += 1
-                    
-                    p = p/count
-                    
-                    summ = 0
+                    old_action = policy[a,b,c,d]
                     for sa in range(3):
                         for sb in range(3):
                             for sc in range(6):
                                 for sd in range(6):
-                                    summ += p[sa,sb,sc,sd]*(-1 + gamma*V_value_table[sa,sb,sc,sd])
-                            
-                    
-                    V_value_table[a,b,c,d] = summ
-                    
-                    delt = max(delt, abs(v - V_value_table[a,b,c,d]))
+                                    pi_right = np.zeros(shape)
+                                    pi_left = pi_right.copy()
+                                    pi_right[a,b,c,d] = p[sa,sb,sc,sd, 1]*(1 + discount*V_value_table[sa,sb,sc,sd]) # 1 is right
+                                    pi_left[a,b,c,d] = p[sa,sb,sc,sd, 0]*(1 + discount*V_value_table[sa,sb,sc,sd])  # 0 is left
+                                    policy[a,b,c,d] = np.argmax([pi_right[sa,sb,sc,sd], pi_left[sa, sb,sc,sd]])
+                                    if old_action != policy[a,b,c,d]: policy_stable = False; difference_count+=1
+    print(difference_count)
+    if policy_stable is True:
+        break   # the optimal policy is obtained. Break and return.
+    else:
+        Evaluation()
 
-
-
-        #env.render()
-        action = policy(obs)
-        obs, reward, done, info = env.step(action)
-        episode_reward += reward
-        if done:
-            break
-    totals.append(episode_reward)
-env.close()
+print(policy, V_value_table)
+print(eposoid)
